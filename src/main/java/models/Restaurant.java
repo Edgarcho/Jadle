ffeba47a617332d8a954371b7539d6533dbe8e5d
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-28 11:23:24 -0800

    Added properties to Restaurant object

--- src/main/java/models/Restaurant.java
+++ src/main/java/models/Restaurant.java
@@ -1,5 +1,11 @@
 package models;
 
 public class Restaurant {
-
+    private String name;
+    private String address;
+    private String zipcode;
+    private String phone;
+    private String website;
+    private String email;
+    private int id;
 }
